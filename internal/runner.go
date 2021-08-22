11c94a452c7cc033bd467bcbde81d7608f292c1c
Harry Zheng <harry@harryzheng.com>
2021-08-21 20:14:56 -0400

    Support adding tasks to cron runner

---
+++ internal/runner.go
@@ -0,0 +1,48 @@
+package internal
+
+import (
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/robfig/cron/v3"
+)
+
+var cronRunner *cron.Cron
+
+func init() {
+	cronRunner = cron.New()
+}
+
+func parseCommand(command string) (name string, args []string) {
+	args = strings.Split(command, " ")
+	name = args[0]
+	args = args[1:]
+	return name, args
+}
+
+func runCommand(command string) {
+	log.Println("running command:", command)
+	name, args := parseCommand(command)
+	cmd := exec.Command(name, args...)
+	out, err := cmd.CombinedOutput()
+	log.Println("finish running command:", command)
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	log.Printf("combined out:\n%s\n", string(out))
+}
+
+func RunInitialSetup(tasks []Task) {
+	for _, task := range tasks {
+		cronRunner.AddFunc(task.Schedule, func() {
+			runCommand(task.Command)
+		})
+	}
+}
+
+func RunAdditionalTask(task Task) {
+	cronRunner.AddFunc(task.Schedule, func() {
+		runCommand(task.Command)
+	})
+}
