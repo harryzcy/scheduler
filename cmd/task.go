b1b619f9bbd6a6090010ab843fcec11fbabe8884
Harry Zheng <harry@harryzheng.com>
2021-09-15 19:31:32 -0400

    Add task command

---
+++ cmd/task.go
@@ -0,0 +1,14 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(taskCmd)
+}
+
+var taskCmd = &cobra.Command{
+	Use:   "task",
+	Short: "Manage tasks",
+}
