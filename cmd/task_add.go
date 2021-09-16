363a215384978d130b59054d2e90adcb8c9f2f6a
Harry Zheng <harry@harryzheng.com>
2021-09-16 16:34:57 -0400

    Support adding task at client side

---
+++ cmd/task_add.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/harryzcy/scheduler/internal/messenger"
+	"github.com/spf13/cobra"
+)
+
+var (
+	name     string
+	command  string
+	schedule string
+	once     bool
+)
+
+func init() {
+	taskCmd.AddCommand(taskAddCmd)
+
+	taskAddCmd.Flags().StringVarP(&name, "name", "n", "", "Name of the task")
+	taskAddCmd.Flags().StringVarP(&command, "command", "c", "", "Command to run")
+	taskAddCmd.Flags().StringVarP(&schedule, "schedule", "s", "", "Schedule of the command")
+	taskAddCmd.Flags().BoolVarP(&once, "once", "o", false, "Run the command only once")
+}
+
+var taskAddCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a new task",
+	Run: func(cmd *cobra.Command, args []string) {
+		if name == "" {
+			fmt.Println("Error: missing flag: --name, -n")
+			cmd.Usage()
+		}
+		if command == "" {
+			fmt.Println("Error: missing flag: --command, -c")
+			cmd.Usage()
+		}
+		if schedule == "" {
+			fmt.Println("Error: missing flag: --schedule, -s")
+			cmd.Usage()
+		}
+
+		messenger.AddTask(name, command, schedule, once)
+	},
+}
--- internal/messenger/client.go
+++ internal/messenger/client.go
@@ -3,7 +3,6 @@ package messenger
 import (
 	"context"
 	"log"
-	"os"
 	"time"
 
 	pb "github.com/harryzcy/scheduler/internal/messenger/messenger"
@@ -15,23 +14,25 @@ const (
 	defaultName = "world"
 )
 
-func AddTask() {
+func AddTask(name, command, schedule string, once bool) {
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("failed to connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewMessengerClient(conn)
 
-	// Contact the server and print out its response.
-	name := defaultName
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	_, err = c.AddTask(ctx, &pb.Task{Name: name})
+
+	task := &pb.Task{
+		Name:     name,
+		Command:  command,
+		Schedule: schedule,
+		Once:     once,
+	}
+	_, err = c.AddTask(ctx, task)
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		log.Fatalf("failed to add the task: %v", err)
 	}
 }
